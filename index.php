0ab853c0a723dc9729fca5aa85e9abbc2c714b00
Andrew R Emery <hello@andrewremery.com>
2022-09-03 22:21:04 -0400

    :sparkles: Placeholder content

--- index.php
+++ index.php
@@ -12,33 +12,181 @@
 <body>
 	<main>
 		<article id="title">
-
+			<section>
+				<h1>Lorem ipsum dolor sit amet</h1>
+			</section>
+			<section>
+				<!--PUT MODEL HERE-->
+			</section>
 		</article>
 
-    <article id="why">
-      
+		<article id="why">
+			<h2>Lorem ipsum dolor sit amet</h2>
+			<section>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+					et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+					aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+					culpa qui officia deserunt mollit anim id est laborum.</p>
+			</section>
 		</article>
 
-    <article id="steps">
-      
+		<article id="steps">
+			<h2>Lorem ipsum dolor sit amet</h2>
+			<section>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+					et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+					aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+					culpa qui officia deserunt mollit anim id est laborum.</p>
+			</section>
+			<section>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+					et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+					aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+					culpa qui officia deserunt mollit anim id est laborum.</p>
+			</section>
+			<section>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+					et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+					aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+					culpa qui officia deserunt mollit anim id est laborum.</p>
+			</section>
+			<section>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+					et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+					aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+					culpa qui officia deserunt mollit anim id est laborum.</p>
+			</section>
+			<section>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+					et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+					aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+					culpa qui officia deserunt mollit anim id est laborum.</p>
+			</section>
 		</article>
 
-    <article id="about">
-      
+		<article id="about">
+			<h2>Lorem ipsum dolor sit amet</h2>
+			<section>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+					et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+					aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+					culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+					et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+					aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+					culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+					et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+					aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+					culpa qui officia deserunt mollit anim id est laborum.</p>
+			</section>
+			<section>
+				<figure>
+					<img src="" alt="Image of van here">
+				</figure>
+			</section>
 		</article>
 
-    <article id="photos">
-      
+		<article id="photos">
+			<h2>Lorem ipsum dolor sit amet</h2>
+			<img src="" alt="Image of van here">
+			<img src="" alt="Image of van here">
+			<img src="" alt="Image of van here">
+			<img src="" alt="Image of van here">
+			<img src="" alt="Image of van here">
 		</article>
 
-    <article id="video">
-      
+		<article id="video">
+			<h2>Lorem ipsum dolor sit amet</h2>
+			<section>
+				<!--VIDEO HERE-->
+			</section>
 		</article>
 
-    <article id="help">
-      
+		<article id="help">
+			<h2>Lorem ipsum dolor sit amet</h2>
+			<section>
+				<h3>Lorem ipsum dolor sit amet</h3>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+			</section>
+			<section>
+				<h3>Lorem ipsum dolor sit amet</h3>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+			</section>
+			<section>
+				<h3>Lorem ipsum dolor sit amet</h3>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+			</section>
+			<section>
+				<h3>Lorem ipsum dolor sit amet</h3>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+				<li><img src="" alt="image of person here"></li>
+			</section>
 		</article>
-
 	</main>
 	<script src="_js/_vendors/jquery.js"></script>
 	<script src="_js/scripts.js"></script>
