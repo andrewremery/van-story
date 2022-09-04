63ff00190be67b93507a0694012ae43de99f8399
Andrew R Emery <hello@andrewremery.com>
2022-09-03 22:09:54 -0400

    :sparkles: Setting up sections

--- index.php
+++ index.php
@@ -1,15 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Van Story</title>
-    <link rel="stylesheet" href="main.css">
-  </head>
-  <body>
-	<h1>Test</h1>
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Van Story</title>
+	<link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+	<main>
+		<article id="title">
+
+		</article>
+
+    <article id="why">
+      
+		</article>
+
+    <article id="steps">
+      
+		</article>
+
+    <article id="about">
+      
+		</article>
+
+    <article id="photos">
+      
+		</article>
+
+    <article id="video">
+      
+		</article>
+
+    <article id="help">
+      
+		</article>
+
+	</main>
 	<script src="_js/_vendors/jquery.js"></script>
 	<script src="_js/scripts.js"></script>
-  </body>
+</body>
+
 </html>
