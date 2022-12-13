48ff2a3cb0e05d628fe62548c1c067a785789237
Andrew R Emery <hello@andrewremery.com>
2022-12-12 20:07:31 -0700

    :sparkles: FAQ improvements

--- _js/scripts.js
+++ _js/scripts.js
@@ -21,8 +21,29 @@ $(document).ready(function () {
   });
 
   $(".accordion").accordion({
-    active: 2,
-    collapsible: true,
-    animate: 200,
+    active: true,
+    //collapsible: true,
+    animate: 500,
+    heightStyle: "content",
+  });
+  /*Hack to make only one accordion open at a time
+THIS SHOULD BE CHANGED. IT IS NOT TOGGLING ARIA ATTRIBUTES SO IT IS VERY BAD FOR ACCESSIBILITY
+*/
+  $(".accordion").click(function () {
+    $(".accordion").each(function () {
+      // On click make every accorion hidden
+      $(".ui-accordion-header").removeClass("ui-state-active");
+      $(".ui-accordion-header").removeClass("ui-accordion-header-active");
+      $(".ui-accordion-content").removeClass("ui-accordion-content-active");
+      $(".ui-accordion-content").css("display", "none");
+    });
+    //Then turn back on only the current one
+    var currentHeader = $(this).find(".ui-accordion-header")[0];
+    var currentContent = $(this).find(".ui-accordion-content")[0];
+
+    $(currentHeader).addClass("ui-state-active");
+    $(currentHeader).addClass("ui-accordion-header-active");
+    $(currentContent).addClass("ui-accordion-content-active");
+    $(currentContent).css("display", "block");
   });
 });
